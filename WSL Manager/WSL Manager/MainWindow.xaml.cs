e2f9186427ae29a4bce9c4892b678cf910a9f19d
Moon <rycmoon@gmail.com>
2021-07-27 15:52:14 -0500

    Added a timer to control updating the distros in the background, which updates which buttons should be enabled and updates the state of the selected distro in the select distro field. All buttons have their own function that determine if the button should be enabled. These functions are run by the timer. Also a Message box to prevent usage of the program when WSL distros are open prior to launch.

--- WSL Manager/WSL Manager/MainWindow.xaml.cs
+++ WSL Manager/WSL Manager/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Timers;
 using System.Windows;
 
 
@@ -12,9 +13,39 @@ namespace WSL_Manager
     /// </summary>
     public partial class MainWindow : Window
     {
+        
         List<string[]> _distroList = new List<string[]>();
         private Dictionary<string, Distro> distros = new Dictionary<string, Distro>();
+        
+        private bool displayGuiTools = false;
+        
+        #region TIMER/UPDATE
+        
+        private System.Timers.Timer timer1;
+        private void UpdateFormsTimer()
+        {
+            double seconds = .1 *1000;
+            timer1 = new System.Timers.Timer(seconds);
+            timer1.Elapsed += UpdateFormsFunction;
+            timer1.AutoReset = true;
+            timer1.Enabled = true;
+        }
 
+        /// <summary>
+        /// Allows multithreading so that the timer can continuosly
+        /// check to make sure that the buttons and list of distros
+        /// are handled properly
+        /// </summary>
+        /// <param name="source"></param>
+        /// <param name="e"></param>
+        private void UpdateFormsFunction(Object source, ElapsedEventArgs e)
+        {
+            Dispatcher.Invoke(() =>
+            {
+                EnableDisableButtons();
+                UpdateSelectedDistroField();
+            });
+        }
         
         
         //TODO: INSTALL RDP
@@ -167,78 +198,32 @@ namespace WSL_Manager
         /// <summary>
         /// This disabled the GUI buttons should the Distro selected not be version 2
         /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        private void WhenToDisplayButtons(object sender, EventArgs e)
+        private void EnableDisableButtons()
         {
-            string distro = GetSelectedDistro();
-            string logInStyle = LogInMethod();
-
-            bool properDistroSelected = !distro.Contains("Select");
-            bool properLoginSelected = !logInStyle.Contains("Select");
-            bool properIsRunning = ListOfDistros.SelectedItem.ToString().Contains("Running");
-
-            if (properDistroSelected)
-            {
-                LogInAs.IsEnabled = true;
-                //If you can upgrade your WSL
-                int wslVersion = GetSelectedDistroVersionNumber();
-
-                if (2 == wslVersion)
-                {
-                    UpgradeDistroButton.IsEnabled = false;
-                    InstallGuiTools.IsEnabled = true;
-                    OpenDistroGui.IsEnabled = true;
-                }
-                else
-                {
-                    UpgradeDistroButton.IsEnabled = true;
-                    InstallGuiTools.IsEnabled = false;
-                    OpenDistroGui.IsEnabled = false;    
-                }
-            }
-            else
-            {
-                LogInAs.IsEnabled = false;
-                UpgradeDistroButton.IsEnabled = false;
-                InstallGuiTools.IsEnabled = false;
-                OpenDistroGui.IsEnabled = false;
-                ShutDownSelectedDistro.IsEnabled = false;
-            }
-
-                //Launch Distro
-                if (properDistroSelected && properLoginSelected && !properIsRunning)
-                {
-                    //if it does not display Select && the distro is Stopped
-                    
-                    StartSelectedDistroButton.IsEnabled = true;
-                    ShutDownAllDistro.IsEnabled = true;
-                }
-                else
-                {
-                    StartSelectedDistroButton.IsEnabled = false;
-                }
-
-                if (properIsRunning)
-                {
-                    StartSelectedDistroButton.IsEnabled = false;
-                    ShutDownSelectedDistro.IsEnabled = true;
-                }
-                else
-                {
-                    ShutDownSelectedDistro.IsEnabled = false;
-                }
-
-                //If you want to login as a specific user
-                if ((string) LogInAs.SelectedItem == "User")
-                {
-                    UserNameBox.IsEnabled = true;
-                }
-                else
-                {
-                    UserNameBox.IsEnabled = false;
-                }
             
+            //Can Select Login Method
+            LogInAs.IsEnabled = canSelectLogin();
+         
+            //Can Type Username
+            UserNameBox.IsEnabled = canTypeUsername();
+            
+            //Can Launch Selected Distro
+            StartSelectedDistroButton.IsEnabled = canLaunchDistro();
+            
+            //Can Upgrade to WSL2
+            UpgradeDistroButton.IsEnabled = canUpgradeToWsl2();
+                
+            //Can Install GUI TOOLS
+            InstallGuiTools.IsEnabled = displayGuiTools;//canInstallGuiTools();
+            
+            //Can Open Distro Gui
+            OpenDistroGui.IsEnabled = displayGuiTools;//canOpenDistroGUI();
+            
+            //Can Shut Down Selected Gui
+            ShutDownSelectedDistro.IsEnabled = canShutDownSelectedDistro();
+            
+            //Shut down all distros
+            ShutDownAllDistro.IsEnabled = isADistroRunning();
         }
 
         /// <summary>
@@ -320,6 +305,214 @@ namespace WSL_Manager
                 
                 _distroList.Add( new[] {modifiedLine[1], modifiedLine[2], modifiedLine[3]} );
             }
+        }
+        
+        /// <summary>
+        /// Updates the selected Distro for when
+        /// its changing from running to not running or visa versa 
+        /// </summary>
+        private void UpdateSelectedDistroField()
+        {
+            string distro = GetSelectedDistro();
+            bool oldProperIsRunning = ListOfDistros.SelectedItem.ToString().Contains("Running");
+            
+            bool properDistroSelected = !distro.ToString().Contains("Select");
+            if (properDistroSelected)
+            {
+                RefreshDistroList();
+                foreach (string[] distroProperties in _distroList)
+                {
+                    bool newProperRunning = distroProperties[1].Contains("Running");
+                    if (distro == distroProperties[0] && oldProperIsRunning != newProperRunning)
+                    {
+                        ListOfDistros.Items.Add(distro + " " + distroProperties[1]);
+                        ListOfDistros.SelectedItem = distro + " " + distroProperties[1];
+                    }
+                }
+            }
+        }
+        /// <summary>
+        /// THis runs at startup to ensure that distros launched prior to startup are closed
+        /// </summary>
+        private void AreDistrosRunningOnLaunch()
+        {
+            ListOfDistrosSelectOnClick(null,null);
+            string message = "ERROR:\n" +
+                             "\n" + 
+                             "A Linux Distro was running prior to launch.\n" +
+                             "\n" +
+                             "OPTIONS: \n" +
+                             "\n" +
+                             "1) You can select \"Yes\" to close all active WSL distros and proceed.\n" +
+                             "2) You can select \"No\" to close the program and and manually close out of all WSL distros yourself.\n" +
+                             "\n" +
+                             "Would You like to Force Close all distros and continue?";
+            
+            if (isADistroRunning())
+            {
+                if (MessageBox.Show(this, message, "ERROR", MessageBoxButton.YesNo) == MessageBoxResult.Yes)
+                {
+                    ManageDistros.ShutDownAllDistros();
+                }
+                else
+                {
+                    System.Environment.Exit(0);  
+                }
+                
+            }
+        }
+        
+        #endregion
+        
+        #region WHEN TO DISPLAY BUTTONS
+        
+        /// <summary>
+        /// If a distro is running return true
+        /// This is used to control wether or not you should enable the shut all distros down button
+        /// </summary>
+        /// <returns></returns>
+        private bool isADistroRunning()
+        {
+            foreach (string[] distroProperties in _distroList)
+            {
+                bool isRunning = distroProperties[1].Contains("Running");
+                if (isRunning)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+
+        }
+        /// <summary>
+        /// If a proper distro is selected return true
+        /// </summary>
+        /// <returns></returns>
+        private bool canSelectLogin()
+        {
+            if (ListOfDistros.SelectedItem.ToString().Contains("Select"))
+            {
+                return false;
+            }
+
+            return true;
+        }
+        /// <summary>
+        /// If a proper login method and a proper distro are selected return true
+        /// </summary>
+        /// <returns></returns>
+        private bool canLaunchDistro()
+        {
+            string distro = GetSelectedDistro();
+            string logInStyle = GetSelectedLogInMethod();
+
+            bool properDistroSelected = !distro.Contains("Select");
+            bool properLoginSelected = !logInStyle.Contains("Select");
+            bool properIsRunning = ListOfDistros.SelectedItem.ToString().Contains("Running");
+
+            if (properDistroSelected && properLoginSelected && !properIsRunning)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+            
+        }
+        /// <summary>
+        /// Can use textbox to type name if User is selcted
+        /// </summary>
+        /// <returns></returns>
+        private bool canTypeUsername()
+        {
+            //If you want to login as a specific user
+            if (LogInAs.SelectedItem.ToString().Contains("User") && canSelectLogin())
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        /// <summary>
+        /// Checks if the selected distro is WSL2
+        /// if it is not version 2 it returns true
+        /// </summary>
+        /// <returns></returns>
+        private bool canUpgradeToWsl2()
+        {
+            if (canSelectLogin() && !canShutDownSelectedDistro())
+            {
+                string distro = GetSelectedDistro();
+                foreach (string[] distroProperties in _distroList)
+                {
+                    bool isWSL2 = distroProperties[2].Contains("2");
+                    if (distro == distroProperties[0] && !isWSL2)
+                    {
+                        return true;
+                    }
+                }
+            }
+
+            return false;
+        }
+        /// <summary>
+        /// If distro is WSL2 return true
+        /// </summary>
+        /// <returns></returns>
+        private bool canInstallGuiTools()
+        {
+            if (canSelectLogin() && canShutDownSelectedDistro())
+            {
+                string distro = GetSelectedDistro();
+                foreach (string[] distroProperties in _distroList)
+                {
+                    bool isWSL2 = distroProperties[2].Contains("2");
+                    if (distro == distroProperties[0] && isWSL2)
+                    {
+                        return true;
+                    }
+                }
+            }
+
+            return false;
+        }
+        /// <summary>
+        /// If distro is WSL2 return true
+        /// </summary>
+        /// <returns></returns>
+        private bool canOpenDistroGUI()
+        {
+            if (canSelectLogin() && canShutDownSelectedDistro())
+            {
+                string distro = GetSelectedDistro();
+                foreach (string[] distroProperties in _distroList)
+                {
+                    bool isWSL2 = distroProperties[2].Contains("2");
+                    if (distro == distroProperties[0] && isWSL2)
+                    {
+                        return true;
+                    }
+                }
+            }
+
+            return false;
+        }
+        /// <summary>
+        /// Returns true is selected distro displays running
+        /// </summary>
+        /// <returns></returns>
+        private bool canShutDownSelectedDistro()
+        {
+            if (ListOfDistros.SelectedItem.ToString().Contains("Running"))
+            {
+                return true;
+            }
+
+            return false;
         }
         
         #endregion
