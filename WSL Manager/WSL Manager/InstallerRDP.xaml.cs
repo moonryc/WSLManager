229624fc68e4b5b3238da71dab5d9719c6a1aa9d
Moon <rycmoon@gmail.com>
2021-07-31 15:31:54 -0500

    first commit the installer window, it should boot to page 1 but for quick debugging it current will load straight to page 2

---
+++ WSL Manager/WSL Manager/InstallerRDP.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+
+namespace WSL_Manager
+{
+    /// <summary>
+    ///     Interaction logic for PopUpWindow.xaml
+    /// </summary>
+    public partial class InstallerRDP : Window
+    {
+        public InstallerRDP()
+        {
+            InitializeComponent();
+            OnWindowLoad();
+        }
+
+        private void OnWindowLoad()
+        {
+            //Display First Frame
+            InstallerFrame.NavigationService.Navigate(new InstallPage2());
+        }
+    }
+}
