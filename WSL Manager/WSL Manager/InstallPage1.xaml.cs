47a9ae490b064324402018448d2e489bf5f70785
Moon <rycmoon@gmail.com>
2021-07-31 15:31:06 -0500

    first commit page one, need to create a walkthrough version of how to install and configure xrdp for those who want to do it manually. Also need to add functionallity to the ttodo button

---
+++ WSL Manager/WSL Manager/InstallPage1.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+
+namespace WSL_Manager
+{
+    /// <summary>
+    /// Interaction logic for InstallPage1.xaml
+    /// </summary>
+    public partial class InstallPage1 : Page
+    {
+        
+        /// <summary>
+        /// Closes GUI installer
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        /// <exception cref="NotImplementedException"></exception>
+        private void ClosePageOne(object sender, RoutedEventArgs e)
+        {
+            //TODO CLOSE PAGE ONE BUTTON
+        }
+        
+        //TODO GUIDE ME WALKTHROUGH
+        //TODO GUIDE ME PAGE ONE BUTTON
+        /// <summary>
+        /// Takes you to the Gui Installer walkthrough rather than automation
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        /// <exception cref="NotImplementedException"></exception>
+        private void GuideMePageOne(object sender, RoutedEventArgs e) {
+            MainWindow returnToWSLManager = new MainWindow();
+            returnToWSLManager.Show();
+        }
+        
+        /// <summary>
+        /// Takes you to Page 2
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ContinuePageOne(object sender, RoutedEventArgs e)
+        {
+            this.NavigationService.Navigate(new InstallPage2());
+        }
+        
+        
+        public InstallPage1()
+        {
+            InitializeComponent();
+        }
+        
+    }
+}
