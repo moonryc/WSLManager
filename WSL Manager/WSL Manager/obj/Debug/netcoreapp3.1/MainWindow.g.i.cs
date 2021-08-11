eeaae16af21c2ebc894d07c2cb873ffd3b3ef8c9
moon <rycmoon@gmail.com>
2021-08-11 16:37:02 -0500

    auto-generated

--- WSL Manager/WSL Manager/obj/Debug/netcoreapp3.1/MainWindow.g.i.cs
+++ WSL Manager/WSL Manager/obj/Debug/netcoreapp3.1/MainWindow.g.i.cs
@@ -132,7 +132,7 @@ namespace WSL_Manager {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/WSL Manager;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/WSL Manager;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
