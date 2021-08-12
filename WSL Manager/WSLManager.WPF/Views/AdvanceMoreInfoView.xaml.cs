cbe64d357dfd089f69c6c11deb82b5f107ba1457
moon <rycmoon@gmail.com>
2021-08-12 17:19:01 -0500

    First commit Views

---
+++ WSL Manager/WSLManager.WPF/Views/AdvanceMoreInfoView.xaml.cs
@@ -0,0 +1,14 @@
+using MvvmCross.Platforms.Wpf.Presenters.Attributes;
+using MvvmCross.Platforms.Wpf.Views;
+
+namespace WSLManager.WPF.Views
+{
+    [MvxContentPresentation]
+    public partial class AdvanceMoreInfoView : MvxWpfView
+    {
+        public AdvanceMoreInfoView()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ WSL Manager/WSLManager.WPF/Views/UnRegisterADistroView.xaml.cs
@@ -0,0 +1,14 @@
+using MvvmCross.Platforms.Wpf.Presenters.Attributes;
+using MvvmCross.Platforms.Wpf.Views;
+
+namespace WSLManager.WPF.Views
+{
+    [MvxContentPresentation]
+    public partial class UnRegisterADistroView : MvxWpfView
+    {
+        public UnRegisterADistroView()
+        {
+            InitializeComponent();
+        }
+    }
+}
