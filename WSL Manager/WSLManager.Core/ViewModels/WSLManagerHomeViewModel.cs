b2154940602c376af1510111b6b123b5e3dc0109
moon <rycmoon@gmail.com>
2021-08-14 17:09:02 -0500

    added threading to update the combo box regarding the distro status in the background. Reorganized the navigation. fixed some possible Distro objects being null issues.  updating the combobox ui using threading was accomplished by using Binding Operations. I plan on ccommenting on this code tomorrow, its been a long week.

--- WSL Manager/WSLManager.Core/ViewModels/WSLManagerHomeViewModel.cs
+++ WSL Manager/WSLManager.Core/ViewModels/WSLManagerHomeViewModel.cs
@@ -1,6 +1,8 @@
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Threading;
 using System.Threading.Tasks;
+using System.Windows.Data;
 using MvvmCross.Commands;
 using MvvmCross.Navigation;
 using MvvmCross.ViewModels;
@@ -10,23 +12,27 @@ using WSLManager.Core.ViewModels.Installer;
 
 namespace WSLManager.Core.ViewModels
 {
-    public class WSLManagerHomeViewModel:MvxViewModel
-    {
 
+    public class WSLManagerHomeViewModel:MvxViewModel<DistroInstanceBankNavigationArgs>
+    {
+        private bool updateDistroList = true;
+        private DistroModel _selectedDistro;
         private ObservableCollection<DistroModel> _distroListComboBox = new ObservableCollection<DistroModel>();
-        private ObservableCollection<string> _userListComboBox = new ObservableCollection<string>(){"Select User","Default User","root","User" };
-        
         private string _selectedUser;
-        
+        private ObservableCollection<string> _userListComboBox = new ObservableCollection<string>(){"Select User","Default User","root","User" };
+        private int _selectedDistroIndex = 0;
+        public int SelectedDistroIndex { get=>_selectedDistroIndex; set=> SetProperty(ref _selectedDistroIndex,value); }
+        private object _lockObject = new object();
         
         private string _loginUsername;
-        private Dictionary<string, DistroInstance> _storeDistroInstances = new Dictionary<string, DistroInstance>();
+        private Dictionary<string, DistroInstance> _storeDistroInstances;
         
             
         public ObservableCollection<DistroModel> DistroListComboBox { get=>_distroListComboBox; set=>SetProperty(ref _distroListComboBox,value); }
         public ObservableCollection<string> UserListComboBox { get=>_userListComboBox; set=>SetProperty(ref _userListComboBox,value); }
-        private DistroModel _selectedDistro;
-        
+
+        #region Buttons
+
         #region Button On/Off props/getters
         
         private bool _isEnabledSelectUser;
@@ -42,13 +48,13 @@ namespace WSLManager.Core.ViewModels
             set
             {
                 SetProperty(ref _selectedDistro, value);
-                if (!_selectedDistro.DistroListItem.Contains("Select"))
+                if (_selectedDistro == null || _selectedDistro.DistroListItem.Contains("Select"))
                 {
-                    IsEnabledSelectUser = true;
+                    IsEnabledSelectUser = false;
                 }
                 else
                 {
-                    IsEnabledSelectUser = false;
+                    IsEnabledSelectUser = true;
                 }
                 ButtonUpdates();
             }
@@ -93,7 +99,7 @@ namespace WSLManager.Core.ViewModels
         
         private void OnOffSelectUser()
         {
-            if (SelectedDistro.DistroListItem.Contains("Select"))
+            if (SelectedDistro == null || SelectedDistro.DistroListItem.Contains("Select"))
             {
                 IsEnabledSelectUser = false;
                 IsEnabledUserName = false;
@@ -107,63 +113,78 @@ namespace WSLManager.Core.ViewModels
         }
         private void OnOffUsername()
         {
-            if (SelectedUser.Equals("User"))
-            {
-                IsEnabledUserName = true;
-            }
-            else
+            if(SelectedUser == null)
             {
                 IsEnabledUserName = false;
             }
+            else
+            {
+                if (SelectedUser.Equals("User"))
+                {
+                    IsEnabledUserName = true;
+                }
+                else
+                {
+                    IsEnabledUserName = false;
+                }
+            }
+            
         }
         private void OnOffStartDistro()
         {
-            switch (SelectedUser)
+            if (SelectedDistro == null || SelectedDistro.DistroStatus == null || SelectedDistro.DistroStatus.Equals("Running"))
             {
-                case "Select User":
-                    IsEnabledStartSelectedDistro = false;
-                    break;
-                case "Default User":
-                    if (SelectedDistro.DistroListItem.Contains("Select"))
-                    {
+                IsEnabledStartSelectedDistro = false;
+            }
+            else
+            {
+                switch (SelectedUser)
+                {
+                    case "Select User":
                         IsEnabledStartSelectedDistro = false;
-                    }
-                    else
-                    {
-                        IsEnabledStartSelectedDistro = true;
-                    }
-                    break;
-                case "root":
-                    if (SelectedDistro.DistroListItem.Contains("Select"))
-                    {
-                        IsEnabledStartSelectedDistro = false;
-                    }
-                    else
-                    {
-                        IsEnabledStartSelectedDistro = true;
-                    }
-                    break;
-                case "User":
-                    if (string.IsNullOrEmpty(LoginUsername) || string.IsNullOrWhiteSpace(LoginUsername) || SelectedDistro.DistroListItem.Contains("Select"))
-                    {
-                        IsEnabledStartSelectedDistro = false;    
-                    }
-                    else
-                    {
-                        IsEnabledStartSelectedDistro = true;
-                    }
-                    break;
+                        break;
+                    case "Default User":
+                        if (SelectedDistro.DistroListItem.Contains("Select"))
+                        { 
+                            IsEnabledStartSelectedDistro = false;
+                        }
+                        else
+                        { 
+                            IsEnabledStartSelectedDistro = true;
+                        }
+                        break;
+                    case "root":
+                        if (SelectedDistro.DistroListItem.Contains("Select"))
+                        { 
+                            IsEnabledStartSelectedDistro = false;
+                        }
+                        else
+                        { 
+                            IsEnabledStartSelectedDistro = true;
+                        }
+                        break;
+                    case "User":
+                        if (string.IsNullOrEmpty(LoginUsername) || string.IsNullOrWhiteSpace(LoginUsername) || SelectedDistro.DistroListItem.Contains("Select"))
+                        { 
+                            IsEnabledStartSelectedDistro = false;    
+                        }
+                        else
+                        { 
+                            IsEnabledStartSelectedDistro = true;
+                        }
+                        break;
+                }
             }
         }
         private void OnOffCloseSelectedDistro()
         {
-            if (SelectedDistro.DistroStatus.Equals("Running"))
+            if (SelectedDistro == null || SelectedDistro.DistroStatus == null  || !SelectedDistro.DistroStatus.Equals("Running"))
             {
-                IsEnabledCloseSelectedDistro = true;
+                IsEnabledCloseSelectedDistro = false;
             }
             else
             {
-                IsEnabledCloseSelectedDistro = false;
+                IsEnabledCloseSelectedDistro = true;
             }
         }
         private void OnOffCloseAllDistros()
@@ -171,7 +192,7 @@ namespace WSLManager.Core.ViewModels
             bool temp = false;
             foreach (DistroModel distro in _distroListComboBox)
             {
-                if (distro.DistroStatus.Equals("Running"))
+                if (distro.DistroStatus !=null && distro.DistroStatus.Equals("Running"))
                 {
                     temp = true;
                     break;
@@ -218,122 +239,151 @@ namespace WSLManager.Core.ViewModels
                     _storeDistroInstances[SelectedDistro.DistroName].StartDistroInstanceUser(LoginUsername);
                     break;
             }
-            //IsEnabledCloseSelectedDistro = true;
         }        
 
         private async Task CloseSelectedDistro()
         {
             _storeDistroInstances[SelectedDistro.DistroName].StopDistroInstance();
-            //Todo MAKE THIS WORK
         }
         
         private async Task CloseAllDistro()
         {
-            //TODO MAKE CLOSE ALL DISTROS WORK
+            foreach (DistroModel distro in _distroListComboBox)
+            {
+                if (distro.DistroVersion!=null && distro.DistroStatus.Equals("Running"))
+                {
+                    _storeDistroInstances[distro.DistroName].StopDistroInstance(); 
+                } 
+            }
         }
         
         #endregion
+
+        #endregion
         
+        #region Prepare/Initialize/View Callbacks
         
-        
+        public override void Prepare(DistroInstanceBankNavigationArgs parameter)
+        {
+            if (parameter.DistroDictionaryBank == null)
+            {
+                _storeDistroInstances = new Dictionary<string, DistroInstance>();
+            }
+            else
+            {
+                _storeDistroInstances = parameter.DistroDictionaryBank;    
+            }
+        }
         
         public override async Task Initialize()
         {
             await base.Initialize();
-
-            //Thread UpdateDistroComboBoxThread = new Thread();
             
-            UpdateDistroComboBox();
             
-            //UpdateDistroComboBoxThread.Start();
-            //await Task.Run(() => { UpdateDistroComboBox(); });
+            BindingOperations.EnableCollectionSynchronization(_distroListComboBox, _lockObject);
+            if (_storeDistroInstances == null)
+            {
+                _storeDistroInstances = new Dictionary<string, DistroInstance>();
+            }
+            
+            OnLoadUpdateDistroComboBox();
+            
+            Thread worker = new Thread(() =>
+            {
+                while (updateDistroList)
+                {
+                    UpdateDistroComboBox();
+                    
+                    Thread.Sleep(5000);
+                    if (!updateDistroList)
+                    {
+                        break;
+                    }
+                }
+            });
+            worker.Name = "List updater";
+            worker.Start();
+            
         }
-
         
+        public override void ViewDisappeared()
+        {
+            base.ViewDisappeared();
+            updateDistroList = false;
+        }
+        #endregion
+
 
         #region ComboBox Distro Methods
         
-        private string RunningOrStoppedConverted(bool isRunning)
+        private void OnLoadUpdateDistroComboBox()
         {
-            if(isRunning){
-                return "Running";
-            }else
+            List<string[]> distroList = DataCommands.GenerateListOfDistros();
+
+            DistroListComboBox.Clear();
+            _distroListComboBox.Add(new DistroModel()
+                {DistroName = "Select Distro", DistroStatus = null, DistroVersion = null});
+
+            foreach (string[] distro in distroList) 
             {
-                return "Stopped";
+                string name = distro[0];
+
+                //if it is an new distro update the dictionary and the combobox
+                if (!_storeDistroInstances.ContainsKey(name))
+                {
+                    _storeDistroInstances.Add(name, new DistroInstance(name));
+                }
+
+                DistroListComboBox.Insert(1,
+                        new DistroModel() {DistroName = name, DistroStatus = distro[1], DistroVersion = distro[2]});
             }
         }
 
         private void UpdateDistroComboBox()
         {
-            _distroListComboBox.Add(new DistroModel(){DistroName = "Select Distro", DistroStatus = null, DistroVersion = null});
-            
-                List<string[]> distroList = DataCommands.GenerateListOfDistros();
-                foreach (string[] distro in distroList)
+            int tempIndexSelection = SelectedDistroIndex;
+            List<string[]> distroList = DataCommands.GenerateListOfDistros();
+
+            ObservableCollection<DistroModel> temp = new ObservableCollection<DistroModel>();
+            temp.Add(new DistroModel()
+                {DistroName = "Select Distro", DistroStatus = null, DistroVersion = null});
+
+            foreach (string[] distro in distroList)
+            {
+                
+                string name = distro[0];
+                
+                //if it is an new distro update the dictionary of distro instances
+                if (!_storeDistroInstances.ContainsKey(name))
                 {
-                    string name = distro[0];
-                    bool status = distro[1].Equals("Running");
-                    int version = int.Parse(distro[2]);
-                    
-                    //if it is an old distro
-                    if (_storeDistroInstances.ContainsKey(name))
-                    {
-                        //
-                        if (_storeDistroInstances[name].IsRunning != status || _storeDistroInstances[name].Version != version)
-                        {
-                            _distroListComboBox.Remove(new DistroModel() {
-                                DistroName = name, 
-                                DistroStatus = RunningOrStoppedConverted(_storeDistroInstances[name].IsRunning),
-                                DistroVersion = $"{_storeDistroInstances[name].Version}"});
-                            _storeDistroInstances[name].IsRunning = status;
-                            _storeDistroInstances[name].Version = version;
-                            _distroListComboBox.Add(new DistroModel() {DistroName = name,DistroStatus = distro[1],DistroVersion = distro[2]});
-                        }
-                        
-                    }
-                    else
-                    {
-                        //if its a new distro
-                        _storeDistroInstances.Add(name, new DistroInstance(name, status,version));
-                        _distroListComboBox.Add(new DistroModel(){DistroName = name,DistroStatus = distro[1],DistroVersion = distro[2]});
-                    }
+                    _storeDistroInstances.Add(name, new DistroInstance(name));
                 }
-        }
 
-        #endregion
-        
-        
+                temp.Insert(1, new DistroModel() {DistroName = name, DistroStatus = distro[1], DistroVersion = distro[2]});
+            }
 
-        #region Advance Tools
-
-        private async Task AdvanceToolsHelp()
-        {
-            await _navigationService.Navigate<AdvanceMoreInfoViewModel>();
+            for (int index = 0; index < temp.Count; index++)
+            {
+                if (temp[index].DistroStatus != DistroListComboBox[index].DistroStatus || temp[index].DistroVersion != DistroListComboBox[index].DistroVersion )
+                {
+                    //DistroListComboBox = temp;
+                    _distroListComboBox[index] = new DistroModel()
+                    {
+                        DistroName = temp[index].DistroName,
+                        DistroStatus = temp[index].DistroStatus,
+                        DistroVersion = temp[index].DistroVersion
+                    };
+                    
+                    
+                    RaisePropertyChanged((() => DistroListComboBox));
+                    RaisePropertyChanged(() => SelectedDistro);
+                }
+            }
+            SelectedDistroIndex = tempIndexSelection;
+            ButtonUpdates();
         }
         
-        private async Task ChangeWslVersion()
-        {
-            await _navigationService.Navigate<ChangeWslVersionViewModel>();
-        }
         
-        private async Task InstallerPage1(){
-            await _navigationService.Navigate<InstallPage1ViewModel>();
-        }
-        
-        private async Task OpenDistroGui()
-        {
-            //TODO MAKE OPEN DISTRO GUI
-        }
-        
-        private async Task ImportDistro()
-        {
-            await _navigationService.Navigate<ImportDistroViewModel>();
-        }
-        
-        private async Task UnregisterDistro()
-        {
-            await _navigationService.Navigate<UnRegisterADistroViewModel>();
-        }
-
         #endregion
 
         #region Navigation
@@ -343,6 +393,36 @@ namespace WSLManager.Core.ViewModels
         public WSLManagerHomeViewModel(IMvxNavigationService navigationService)
         {
             _navigationService = navigationService;
+        }
+        
+        
+        private async Task AdvanceToolsHelp()
+        {
+            await _navigationService.Navigate<AdvanceMoreInfoViewModel, DistroInstanceBankNavigationArgs>(new DistroInstanceBankNavigationArgs(_storeDistroInstances));
+        }
+        
+        private async Task ChangeWslVersion()
+        {
+            await _navigationService.Navigate<ChangeWslVersionViewModel, DistroInstanceBankNavigationArgs>(new DistroInstanceBankNavigationArgs(_storeDistroInstances));
+        }
+        
+        private async Task InstallerPage1(){
+            await _navigationService.Navigate<InstallPage1ViewModel, DistroInstanceBankNavigationArgs>(new DistroInstanceBankNavigationArgs(_storeDistroInstances));
+        }
+        
+        private async Task OpenDistroGui()
+        {
+            //TODO MAKE OPEN DISTRO GUI
+        }
+        
+        private async Task ImportDistro()
+        {
+            await _navigationService.Navigate<ImportDistroViewModel, DistroInstanceBankNavigationArgs>(new DistroInstanceBankNavigationArgs(_storeDistroInstances));
+        }
+        
+        private async Task UnregisterDistro()
+        {
+            await _navigationService.Navigate<UnRegisterADistroViewModel, DistroInstanceBankNavigationArgs>(new DistroInstanceBankNavigationArgs(_storeDistroInstances));
         }
         
         #endregion
