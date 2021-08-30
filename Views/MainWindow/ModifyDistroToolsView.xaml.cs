3862a78532fb425b9098b68fc7f6b3b70a5661f4
moon <rycmoon@gmail.com>
2021-08-30 15:43:09 -0500

    creates the installer window

---
+++ Views/MainWindow/ModifyDistroToolsView.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using WSLManager.Windows;
+
+namespace WSLManager.Views.MainWindow
+{
+    public partial class ModifyDistroToolsView : UserControl
+    {
+        public ModifyDistroToolsView()
+        {
+            InitializeComponent();
+        }
+
+        private void GuIButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            InstallerWindow newInstallerWindow = new InstallerWindow();
+            newInstallerWindow.Show();
+        }
+    }
+}
