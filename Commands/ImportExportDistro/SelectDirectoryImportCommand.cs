cf2b002184d3c5085898192572d244f469cd4462
moon <rycmoon@gmail.com>
2021-09-02 19:08:38 -0500

    first commit commands to import a distro

---
+++ Commands/ImportExportDistro/SelectDirectoryImportCommand.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Windows.Input;
+using WSLManager.ViewModels.ModifyDistroTools;
+
+namespace WSLManager.Commands.ImportExportDistro
+{
+    public class SelectDirectoryImportCommand:ICommand
+    {
+
+
+        private ImportDistroViewModel _viewModel;
+        private System.Windows.Forms.FolderBrowserDialog _folderBrowserDialog;
+        public SelectDirectoryImportCommand(ImportDistroViewModel viewModel)
+        {
+            _viewModel = viewModel;
+        }
+        
+        #region ICommand
+
+        //Occurs when changes occur that affect whether or not the command should execute.
+        event EventHandler ICommand.CanExecuteChanged
+        {
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
+        }
+
+        //Defines the method that determines whether the command can be executed in its current state
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        //Defines the method to be called when the command is invoked
+        public void Execute(object parameter)
+        {
+            _folderBrowserDialog = new System.Windows.Forms.FolderBrowserDialog();
+            _folderBrowserDialog.ShowNewFolderButton = true;
+            
+            _folderBrowserDialog.ShowDialog();
+
+            _viewModel.InstallFilePath = _folderBrowserDialog.SelectedPath;
+
+        }
+
+        #endregion
+    }
+}
---
+++ Commands/ImportExportDistro/SelectDistroToImportCommand.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Windows.Input;
+using Microsoft.Win32;
+using WSLManager.ViewModels.ModifyDistroTools;
+
+namespace WSLManager.Commands.ImportExportDistro
+{
+    public class SelectDistroToImportCommand:ICommand
+    {
+
+        private ImportDistroViewModel _viewModel;
+        private OpenFileDialog _openFileDialog;
+
+        public SelectDistroToImportCommand(ImportDistroViewModel viewModel)
+        {
+            _viewModel = viewModel;
+        }
+        
+        #region ICommand
+
+        //Occurs when changes occur that affect whether or not the command should execute.
+        event EventHandler ICommand.CanExecuteChanged
+        {
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
+        }
+
+        //Defines the method that determines whether the command can be executed in its current state
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        //Defines the method to be called when the command is invoked
+        public void Execute(object parameter)
+        {
+            
+            _openFileDialog = new OpenFileDialog();
+            _openFileDialog.Multiselect = false;
+            _openFileDialog.AddExtension = true;
+
+            Nullable<bool> result = _openFileDialog.ShowDialog(); 
+            
+            if (result == true)
+            {
+                string fullFilePath = _openFileDialog.FileName;
+                _viewModel.FullFilePath = fullFilePath;
+            }
+        }
+
+        #endregion
+    }
+}
