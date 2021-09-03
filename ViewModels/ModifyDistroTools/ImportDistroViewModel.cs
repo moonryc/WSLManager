fdaf297a47872986dcfe90308064e03bcceae3b0
moon <rycmoon@gmail.com>
2021-09-02 19:03:33 -0500

    Importing distros is now possible

--- ViewModels/ModifyDistroTools/ImportDistroViewModel.cs
+++ ViewModels/ModifyDistroTools/ImportDistroViewModel.cs
@@ -1,18 +1,65 @@
 ﻿using System.Windows.Input;
 using WSLManager.Commands;
+using WSLManager.Commands.ImportExportDistro;
 
 namespace WSLManager.ViewModels.ModifyDistroTools
 {
     public class ImportDistroViewModel:BaseViewModel
     {
         private MainWindowViewModel _parent;
+        private string _distroName = "";
+        private string _filePath = "";
+        private string _installFilePath = "Select Install Location";
+        private string _fullFilePath = "Select Distro File";
+        
+        
+        
 
-        public ImportDistroViewModel(MainWindowViewModel parent)
+        // the file path of the distro to be imported
+        public string FullFilePath
         {
-            _parent = parent;
-            UpdateViewCommand = new UpdateViewCommand(_parent);
+            get => _fullFilePath;
+            set
+            {
+                _fullFilePath = value;
+                OnPropertyChanged();
+            }
+        }
+        
+        /// <summary>
+        /// The file path to install the distro
+        /// </summary>
+        public string InstallFilePath
+        {
+            get => _installFilePath;
+            set
+            {
+                _installFilePath = value;
+                OnPropertyChanged();
+            }
+        }
+        
+        /// <summary>
+        /// Get Main window viewmodel (for changing views with the update view command)
+        /// </summary>
+        public MainWindowViewModel MainWindowViewModel { get=>_parent; }
+        
+        /// <summary>
+        /// Gets/sets the custom name of the distro to import
+        /// </summary>
+        public string DistroName
+        {
+            get => _distroName;
+            set
+            {
+                _distroName = value;
+                OnPropertyChanged();
+            }
         }
 
         public ICommand UpdateViewCommand { get; set; }
+        public ICommand ImportDistroCommand { get; set; }
+        public ICommand SelectDistroToImportCommand { get; set; }
+        public ICommand SelectDirectoryImportCommand { get; set; }
     }
 }
