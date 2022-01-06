cfbd031ee881c49ec7207a2e147d68fdd813d41b
moonryc <70920813+moonryc@users.noreply.github.com>
2022-01-06 16:08:58 -0600

    first commit

---
+++ Commands/CloseInstallerWindowCommand.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Windows.Input;
+using WSLManager.ViewModels.Installer;
+
+namespace WSLManager.Commands
+{
+    public class CloseWindowCommand:ICommand
+    {
+        private InstallerWindowViewModel _viewModel;
+        
+        public CloseWindowCommand(InstallerWindowViewModel viewModel)
+        {
+            _viewModel = viewModel;
+        }
+        
+
+        #region ICommand
+
+        //Occurs when changes occur that affect whether or not the command should execute.
+        event EventHandler ICommand.CanExecuteChanged
+        {
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
+        }
+
+        //Defines the method that determines whether the command can be executed in its current state
+        public bool CanExecute(object parameter)
+        {
+            if (_viewModel.CanClose)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        //Defines the method to be called when the command is invoked
+        public void Execute(object parameter)
+        {
+            // _viewModel.CloseWindow(_viewModel.);
+        }
+
+        #endregion
+        
+        
+    }
+}
